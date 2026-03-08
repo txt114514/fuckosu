cabd9d3c259eb2ae3a82e63536d363818a6f3ad3
txt <txt1919810@gmail.com>
2026-03-08 11:56:42 +0800

    init

--- test_gpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-print("Torch version:", torch.__version__)
-print("CUDA version:", torch.version.cuda)
-print("CUDA available:", torch.cuda.is_available())
-print("GPU count:", torch.cuda.device_count())
-
-if torch.cuda.is_available():
-    print("GPU name:", torch.cuda.get_device_name(0))
